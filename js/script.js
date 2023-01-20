77b7a8cafbe46906bf7d8e07145b471c22bd2b88
VictoriaT87 <victoriaemt@gmail.com>
2023-01-20 14:14:21 +0000

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-$('p').click(function(){
-    $('p').slideToggle('slow')
-})
+$('button').mouseenter(function() {
+	$(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function() {
+	$(this).removeClass('makeBlue').addClass('makeRed');
+});
