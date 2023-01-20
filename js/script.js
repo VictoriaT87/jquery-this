065f2e80eef819681cb812b6b92af71400c020ac
VictoriaT87 <victoriaemt@gmail.com>
2023-01-20 15:10:15 +0000

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-$('button').mouseenter(function() {
-	$(this).removeClass('makeRed').addClass('makeBlue');
-});
+$(document).ready(function () {
 
-$('button').mouseleave(function() {
-	$(this).removeClass('makeBlue').addClass('makeRed');
+    $('button').mouseenter(function () {
+        $(this).removeClass('makeRed').addClass('makeBlue');
+    });
+
+    $('button').mouseleave(function () {
+        $(this).removeClass('makeBlue').addClass('makeRed');
+    });
+
+    $(".box").on("click", function () {
+        /**
+         * When we click on an element that has
+         * a 'box' class, this event will be fired.
+         */
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
+
 });
